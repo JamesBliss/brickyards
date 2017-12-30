5bb4361441118808e5fb712433e8c138f571f477
James Bliss <james.bliss@lush.co.uk>
2017-12-30 12:16:57 +0000

    remove all

--- resource/js/menu-ck.js
+++
@@ -1,1 +0,0 @@
-var stickerTop=parseInt($(".nav").offset().top);$(window).scroll(function(){$(".nav").css(parseInt($(window).scrollTop())+parseInt($(".nav").css("margin-top"))>stickerTop?{position:"fixed",top:"0px"}:{position:"relative"})});
